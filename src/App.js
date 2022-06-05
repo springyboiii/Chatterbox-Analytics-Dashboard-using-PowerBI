09bf45bc01c83422a87cefb055c9e5b99a2d45b4
springyboiii <sugumararvinth19991018@gmail.com>
2022-06-05 11:45:36 +0530

    new Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
   BrowserRouter,
   
@@ -10,6 +11,15 @@ import Layout from './Layouts/Layout';
 function App() {
   return (
     <BrowserRouter>
+    <link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+
       <Routes>
         <Route path="*" element={<Layout />}>
           {/* <Route index element={<Home />} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import {Navbar,Nav,Container} from 'react-bootstrap';
+// import React from 'react'
+import * as React from 'react';
+import { Tab,Tabs,AppBar } from '@mui/material';
+// import {Navbar,Nav,Container} from 'react-bootstrap';
+import { useState } from 'react';
 const Header = () => {
+  const [Value, setValue] = useState( null)
+  const handleTabs=(e,val)=>{
+    console.log(val)
+setValue(val)  }
+
   return (
     <>
   
-  <Navbar bg="primary" variant="dark">
+  {/* <Navbar bg="primary" variant="dark">
     <Container>
     <Navbar.Brand href="/welcome">ANALYST</Navbar.Brand>
     <Nav className="me-auto">
@@ -15,8 +23,18 @@ const Header = () => {
       <Nav.Link href="#pricing">Predict</Nav.Link>
     </Nav>
     </Container>
-  </Navbar>
+  </Navbar> */}
+<AppBar position="static" onChange={handleTabs}>
+  <Tabs value={Value} onChange={handleTabs}>
+    {/* <Tab label="Item 1"/>
 
+    <Tab label="Item 2"/>
+    <Tab label="Item 3"/> */}
+<Tab label="Statistics" href="/home" />
+  <Tab label="Graphs" href="/graphs" />
+  <Tab label="Predict" href="/predict" />
+  </Tabs>
+</AppBar>
   
 </>
   )}
